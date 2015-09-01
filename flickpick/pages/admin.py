cefcefa64ec491b2619fd537b74cb2f25eaf6620
Zach Calvert (zcalvert) <zcalvert@uoregon.edu>
2015-08-31 21:43:57 -0700

    working Page admin

--- flickpick/pages/admin.py
+++ flickpick/pages/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 
-from models import Page
+from models import Page, MoviesWidget
+
+class WidgetInline(admin.StackedInline):
+	model = Page.widgets_base.through
+	list_display = ('name',)
+	extra = 0
+
 
 class PageAdmin(admin.ModelAdmin):
+	list_display = ('name','get_api_url',)
+
+	inlines = [WidgetInline,]
+
+
+class WidgetAdmin(admin.ModelAdmin):
 	list_display = ('name',)
+
+	
+admin.site.register(MoviesWidget, WidgetAdmin)
 
 admin.site.register(Page, PageAdmin)
--- flickpick/pages/models.py
+++ flickpick/pages/models.py
@@ -41,20 +41,6 @@ class Widget(AbstractWidget):
     start_date = models.DateTimeField(null=True, blank=True, help_text="Time at which this widget will turn on")
     end_date = models.DateTimeField(null=True, blank=True, help_text="Time at which this widget will turn off")
 
-    def cache_modified_date(self):
-        return self.last_modified
-
-    @property
-    def cache_key(self):
-        if not hasattr(self, '_cache_key'):
-            self._cache_key = "widget:{}:{}".format(self.id, self.cache_modified_date())
-        return self._cache_key
-
-    @cache_key.setter
-    def cache_key(self, new_key):
-        self._cache_key = new_key
-
-
     @staticmethod
     def autocomplete_search_fields():
         return ("id__iexact", "name__icontains")
@@ -65,7 +51,7 @@ class Widget(AbstractWidget):
     def get_subclass(self):
         if type(self) != Widget:
             return self
-        return Widget.objects.get_subclass(id=self.id)
+        return MoviesWidget.objects.get(id=self.id)
 
     def __unicode__(self):
         return " ".join([self.name, type(self.get_subclass())._meta.verbose_name])
@@ -78,13 +64,6 @@ class Widget(AbstractWidget):
 
     def type_name_plural(self):
         return self.type_name(plural=True)
-
-    def save(self, *args, **kwargs):
-        super(Widget, self).save(*args, **kwargs)
-        # update modified date on all parent pages when a widget gets changed.
-        #  This intentionally doesn't call page.save -- just updates the last modified date (of any pages that
-        #  need updating)
-        self.pages.filter(last_modified__lt=self.last_modified).update(last_modified=self.last_modified)
 
 
 class WidgetItemManager(models.Manager):
@@ -102,7 +81,6 @@ class WidgetItem(models.Model):
     """
     Abstract base class for items that are contained inside widgets
     """
-    sort_order = models.IntegerField()
     start_date = models.DateTimeField(null=True, blank=True, help_text="Time at which this item will turn on")
     end_date = models.DateTimeField(null=True, blank=True, help_text="Time at which this item will turn off")
 
@@ -115,22 +93,15 @@ class WidgetItem(models.Model):
 class PageToWidget(models.Model):
     widget = models.ForeignKey(Widget, related_name='page_to_widgets')
     page = models.ForeignKey('Page', related_name='page_to_widgets')
-    sort_order = models.IntegerField(default=0)
 
     class Meta:
-        ordering = ['page', 'sort_order']
+        ordering = ['page']
         unique_together = (
             ['page', 'widget'],
         )
 
-    def clean(self):
-        if PageToWidget.objects.exclude(id=self.id).filter(page=self.page, sort_order=self.sort_order).exists():
-            max_order = PageToWidget.objects.filter(page=self.page).aggregate(
-                max_order=models.Max('sort_order'))['max_order']
-            self.sort_order = max_order + 1
-
     def __unicode__(self):
-        return u"{} on {}".format(self.widget, self.page)
+        return "{}".format(self.widget.name)
 
 
 class PageManager(models.Manager):
@@ -141,8 +112,6 @@ class PageManager(models.Manager):
 
     def get_queryset(self):
         qs = super(PageManager, self).get_queryset()
-        if self.filter_defaults is not None:
-            qs = qs.filter(default_for_platform__isnull=not self.filter_defaults)
         return qs
 
 
@@ -165,145 +134,38 @@ class Page(models.Model):
     last_modified = models.DateTimeField(auto_now=True)
     draft = models.BooleanField(default=False)
 
-    default_for_platform = models.CharField(unique=True, null=True, blank=True, choices=(
-        ("mobile", "Mobile Apps"),
-        ("web", "Website"),
-    ), max_length=16)
-
     widgets_base = models.ManyToManyField(
         Widget,
         through=PageToWidget,
-        related_name='pages',
-        limit_choices_to={'default': False}
+        related_name='pages'
     )
 
     objects = PageManager(filter_defaults=False)
     all_objects = PageManager()
 
     class Meta:
-        ordering = ['default_for_platform', 'name']
+        ordering = ['name']
 
     @staticmethod
     def autocomplete_search_fields():
         return ("id__iexact", "name__icontains", "slug__icontains")
 
     @property
-    def cache_key(self):
-        # TODO: this cache key may be awful expensive to build...
-        modified_date = self.last_modified
-
-        # if any widget start or end date has passed since last_modified, we'll need to invalidate the cache key again
-        dates = list(self.widgets_base.values_list('start_date', 'end_date'))
-        dates += AdCarouselItem.objects.filter(carousel__pages=self).values_list('start_date', 'end_date')
-        dates += AdGroupItem.objects.filter(group_widget__pages=self).values_list('start_date', 'end_date')
-        dates = reduce(lambda a, b: list(a) + list(b), dates, [])  # unpack start & end date tuples
-
-        for date in dates:
-            if date is not None and datetime.now() > date > modified_date:
-                modified_date = date
-
-        return "page:{}:{}".format(self.id, modified_date.strftime('%s.%f'))
-
-    @property
-    def default(self):
-        return self.default_for_platform is not None
-
-    @classmethod
-    def default_page_cache_key(cls, platform):
-        return "page::default_instance::"+platform
-
-    @classmethod
-    def get_default_page(cls, platform):
-        default = cache.get(cls.default_page_cache_key(platform))
-        if default is None:
-            try:
-                default = DefaultPage.objects.get(default_for_platform=platform)
-                cache.set(cls.default_page_cache_key(platform), default)
-            except DefaultPage.DoesNotExist:
-                pass
-        return default
-
-    def get_defaulted_instance(self, platform, instance=None):
-        """
-        """
-        default = Page.get_default_page(platform)
-        if default is not None:
-            page = instance or DefaultedPage()
-
-            for field in self._meta.fields:
-                fname = field.attname
-                value = getattr(self, fname)
-                if value in [None, ""]:
-                    value = getattr(default, fname)
-                setattr(page, fname, value)
-            past = datetime(year=1970, month=1, day=1)
-            page.last_modified = max(default.last_modified or past, self.last_modified or past)
-
-            return page
-        else:
-            return self
-
-    def clean(self):
-        if self.default:
-            if self.background_gradient and not self.background_end_color:
-                raise ValidationError({
-                    'background_end_color': [
-                        'This field is required on default instances if background_gradient is checked'
-                    ]
-                })
-
-    @property
     def widgets(self):
-        return self.widgets_base.active().select_subclasses().order_by('page_to_widgets__sort_order')
-
-
-    def movies_displayed(self):
-        movies = []
-        for widget in self.widgets:
-            if isinstance(widget, MoviesWidget):
-                for item in widget.items[:widget.limit]:
-                    movies.append(item)
-        return movies
+        return self.widgets_base.active()
 
     @widgets.setter
     def widgets(self, some_widgets):
         self.widgets_base = some_widgets
 
-    def add_widget(self, widget, sort_order=None):
-        if sort_order is None:
-            sort_order = self.page_to_widgets.aggregate(sort_order=models.Max('sort_order'))['sort_order']
-            if sort_order is None:
-                sort_order = 0
-            else:
-                sort_order += 1
-        return self.page_to_widgets.create(widget=widget, sort_order=sort_order)
+    def add_widget(self, widget):
+        return self.page_to_widgets.create(widget=widget)
 
     def get_api_url(self):
-        if self.slug:
-            return reverse("page", kwargs={"page_slug": self.slug})
-        else:
-            return reverse("page", kwargs={"page_id": self.id})
-
-    def serialize(self, is_hd=False):
-        background_image_dict = {
-            'background_image_url': self.background_image_url,
-        }
-
-        if is_hd:
-            url, ext = os.path.splitext(background_image_dict["background_image_url"])
-            background_image_dict["background_image_url"] = "{0}@2x{1}".format(url, ext)
-
-        return background_image_dict
+        return reverse("page", kwargs={"page_id": self.id})
 
     def __unicode__(self):
         return self.name
-
-    def save(self, *args, **kwargs):
-        result = super(Page, self).save(*args, **kwargs)
-        if self.default:
-            # TODO: can we do this for every page, to pre-emptively cache them?
-            cache.set(self.default_page_cache_key(self.default_for_platform), self)
-        return result
 
 
 class PageLinkMixin(models.Model):
@@ -539,19 +401,17 @@ class MoviesWidget(CatalogGroupWidget):
         if self.source_year:
             movies = movies.filter(year=source.year)
 
-        # if self.new_releases:
-        #     if self.new_releases_window is not None:
-        #         window_start = datetime.now() - timedelta(days=int(self.new_releases_window))
-        #         movies = movies.filter(year__gt=window_start)
-        #     books = books.order_by('-profile__on_sale_date', '-profile__sort_key')
-        # else:
-        #     books = books.order_by('profile__sort_key')
+        if self.new_releases:
+            if self.new_releases_window is not None:
+                window_start = datetime.now() - timedelta(days=int(self.new_releases_window))
+                movies = movies.filter(year__gt=window_start)
+            books = books.order_by('-year')
 
         return books.distinct()
 
     def clean(self):
         super(MoviesWidget, self).clean()
-        if not self.source:
+        if not self.source and not self.new_releases:
             raise ValidationError("A source is required")
 
     def limited_items(self):
@@ -591,31 +451,9 @@ class AdGroupWidget(AbstractGroupWidget):
     def limited_items(self):
         return self.ads.active()
 
-    def cache_modified_date(self):
-        modified_date = super(AdGroupWidget, self).cache_modified_date()
-        dates = self.ads.values_list('start_date', 'end_date')
-        dates = reduce(lambda a, b: list(a) + list(b), dates, [])  # unpack start & end date tuples
-
-        for date in dates:
-            if date is not None and datetime.now() > date > modified_date:
-                modified_date = date
-        return modified_date
-
 
 class BannerWidget(Widget, PageLinkMixin):
     image = models.ImageField()
-    image_avg_color = models.CharField(max_length=10, default='#000000', editable=False)
-    image_aspect_ratio = models.FloatField(default=1.0, editable=False)
-    image_timestamp = models.DateTimeField(null=True, blank=True, editable=False)
-    portrait_alignment = models.CharField(
-        max_length='16',
-        default='center',
-        choices=(
-            ('left', 'Left'),
-            ('center', 'Center'),
-            ('right', 'Right'),
-        )
-    )
     template_name = "widgets/banner.json"
 
     class Meta(Widget.Meta):
@@ -626,20 +464,10 @@ class BannerWidget(Widget, PageLinkMixin):
 class AdGroupItem(PageLinkWidgetItem):
     group_widget = models.ForeignKey(AdGroupWidget, related_name='ads')
     image = models.ImageField()
-    image_avg_color = models.CharField(max_length=10, default='#000000', editable=False)
-    image_aspect_ratio = models.FloatField(default=1.0, editable=False)
-    image_timestamp = models.DateTimeField(null=True, blank=True, editable=False)
 
     class Meta:
         verbose_name = "ad"
         verbose_name_plural = "ads"
-        ordering = ["sort_order"]
-
-    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
-        if self.group_widget and self.sort_order is None:
-            previous_max = self.group_widget.ads.aggregate(o=models.Max('sort_order'))['o'] or 0
-            self.sort_order = previous_max + 1
-        super(AdGroupItem, self).save(force_insert, force_update, using, update_fields)
 
     def clean(self):
         if not self.link:
@@ -653,35 +481,14 @@ class AdCarouselWidget(Widget):
         verbose_name = "carousel of big ads"
         verbose_name_plural = "carousels of big ads"
 
-    def cache_modified_date(self):
-        modified_date = super(AdCarouselWidget, self).cache_modified_date()
-        dates = self.ads.values_list('start_date', 'end_date')
-        dates = reduce(lambda a, b: list(a) + list(b), dates, [])  # unpack start & end date tuples
-
-        for date in dates:
-            if date is not None and datetime.now() > date > modified_date:
-                modified_date = date
-        return modified_date
-
 
 class AdCarouselItem(PageLinkWidgetItem):
     carousel = models.ForeignKey(AdCarouselWidget, related_name='ads')
     image = models.ImageField()
-    image_avg_color = models.CharField(max_length=10, default='#000000', editable=False)
-    image_aspect_ratio = models.FloatField(default=1.0, editable=False)
-    image_timestamp = models.DateTimeField(null=True, blank=True, editable=False)
 
     class Meta:
         verbose_name = "ad"
         verbose_name_plural = "ads"
-        unique_together = ["carousel", "sort_order"]
-        ordering = ["sort_order"]
-
-    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
-        if self.carousel and self.sort_order is None:
-            previous_max = self.carousel.ads.aggregate(o=models.Max('sort_order'))['o'] or 0
-            self.sort_order = previous_max + 1
-        super(AdCarouselItem, self).save(force_insert, force_update, using, update_fields)
 
     def clean(self):
         if not self.link:

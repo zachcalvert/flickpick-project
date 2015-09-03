ed19da4510b177ff9f663b23f476a1157a969098
Zach Calvert <zachc@darkhorse.com>
2015-09-03 13:57:46 -0700

    add people to admin

--- flickpick/movies/admin.py
+++ flickpick/movies/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django import forms
 
-from models import Movie, Director, Actor, Writer, Genre
+from models import Movie, Director, Actor, Writer, Genre, Person
 
 class MovieForm(forms.ModelForm):
     notes = forms.CharField(widget=forms.Textarea)
@@ -38,5 +38,18 @@ class MovieAdmin(admin.ModelAdmin):
 class GenreAdmin(admin.ModelAdmin):
 	list_display = ('name',)
 
+class DirectorAdmin(admin.ModelAdmin):
+	list_display = ('person',)
+
+class ActorAdmin(admin.ModelAdmin):
+	list_display = ('person',)
+
+class WriterAdmin(admin.ModelAdmin):
+	list_display = ('person',)
+
+
+admin.site.register(Director, DirectorAdmin)
+admin.site.register(Actor, ActorAdmin)
+admin.site.register(Writer, WriterAdmin)
 admin.site.register(Genre, GenreAdmin)
 admin.site.register(Movie, MovieAdmin)

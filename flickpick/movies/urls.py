105e482dfe3b7b6ab415d132a58a789cacab6ff3
Zach Calvert <zachc@darkhorse.com>
2015-09-18 14:14:59 -0700

    alphabetize imports

--- flickpick/movies/urls.py
+++ flickpick/movies/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import url
-from django.views.generic import TemplateView
 from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import TemplateView
+
 import views
 
 urlpatterns = [
-    url(r'^$', views.BrowseView.as_view(), name="browse"),
-    url(r'^genres/(?P<genre_id>\d+)/$', views.GenreView.as_view(), name="genre_browse"),
-
     url(r'^seen/$', csrf_exempt(views.SeenMovieView.as_view()), name="user_seen_movie"),
     url(r'^rated/$', csrf_exempt(views.RatedMovieView.as_view()), name="user_rated_movie"),    
 ]
--- flickpick/movies/views.py
+++ flickpick/movies/views.py
@@ -1,17 +1,16 @@
 import json
 
+from django.contrib import messages
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import TemplateView, View
-from django.contrib import messages
 from django.utils.translation import ugettext_lazy as _
+from django.views.generic import TemplateView, View
 from django.views.decorators.csrf import csrf_protect
-from django.contrib.auth.models import User
 
 from models import Movie, Genre
 from viewing.models import Viewing
 
-# Create your views here.
 
 def logout(request, template_name='registration/logged_out.html'):
 	"""
@@ -61,48 +60,3 @@ class RatedMovieView(View):
 
 			d = {'success': 'true', 'movie': '{0} has been rated {1}'.format(movie, rating)}
 			return HttpResponse(json.dumps(d))
-
-
-class BrowseView(TemplateView):
-	template_name = 'browse.html'
-
-	def get_context_data(self, **kwargs):
-		context = super(BrowseView, self).get_context_data(**kwargs)
-
-		action = Genre.objects.get(name='Adventure')
-		action_movies = action.movie_set.order_by('-year')[:12]
-		context['action_movies'] = action_movies
-
-		drama = Genre.objects.get(name='Drama')
-		dramas = drama.movie_set.order_by('-year')[:12]
-		context['dramas'] = dramas
-
-		comedy = Genre.objects.get(name='Comedy')
-		comedies = comedy.movie_set.order_by('-year')[:12]
-		context['comedies'] = comedies
-
-		romance = Genre.objects.get(name='Romance')
-		romance_movies = romance.movie_set.order_by('-year')[:12]
-		context['romance_movies'] = romance_movies
-
-		context['genres'] = Genre.objects.all()
-
-		return context
-
-class GenreView(TemplateView):
-	template_name= 'genre.html'
-
-	def get_context_data(self, **kwargs):
-		context = super(GenreView, self).get_context_data(**kwargs)
-
-		genre = Genre.objects.get(id=kwargs['genre_id'])
-		movies = genre.movie_set.order_by('-year')[:13]
-		award_winners = genre.movie_set.filter(notes__isnull=False)[:13]
-
-		context['genre'] = genre
-		context['movies'] = movies
-		context['award_winners'] = award_winners
-
-		return context
-
-

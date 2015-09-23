f8877cfff10adf43683d95cdcc1f547c710a2d81
Zach Calvert (zcalvert) <zcalvert@uoregon.edu>
2015-09-22 20:05:23 -0700

    no limit

--- flickpick/movies/management/commands/poll_omdb.py
+++ flickpick/movies/management/commands/poll_omdb.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
     Polls the omdb API for movie info
     """
     def handle(self, *args, **options):
-        for title in all_movies[:100]:
+        for title in all_movies:
             response = requests.get('http://www.omdbapi.com/?t={}'.format(title))
 
             if response.status_code == 200:
